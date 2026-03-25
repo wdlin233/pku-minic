40158ff92271415296d522e89f4f02bc43e4b386
wdlin233 <wdlin233@163.com>
2026-03-25 16:24:55 +0800

    chore: better run.sh script

--- run.sh
+++ run.sh
@@ -20,17 +20,23 @@ case "$1" in
     ;;
   test)
     cmake --build build
-    build/compiler $3 $2 -o temp.s
+    build/compiler $2 $3 -o temp.s
+    cat $3
     echo "==> Test finished. Output is temp.s"
     ;;
 
   autotest)
-    echo "==> Running autotest with specific test level $2 with $3 args"
-    autotest $3 -s $2 /root/compiler
+    echo "==> Running autotest with specific test level $3 with $2 args"
+    autotest $2 -s $3 /root/compiler
     ;;
   autotestwd)
-    echo "==> Running autotest with specific test level $2 with $3 and -w args"
-    autotest -w wd $3 -s $2 /root/compiler
+    echo "==> Running autotest with specific test level $3 with $2 and -w args"
+    autotest -w wd $2 -s $3 /root/compiler
+    ;;
+
+  help)
+    echo "Use run.sh test: bash run.sh test -koopa /opt/bin/testcases/lv8/02_params.c"
+    echo "Use run.sh autotest: bash run.sh autotest -koopa lv8"
     ;;
 
   *)
